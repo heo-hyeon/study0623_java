1b9efd957afc38624cf8850ffbf75bd76a6eb747
hyeon <gwg01051@naver.com>
2022-07-28 17:26:36 +0900

    7/28

---
+++ src/com/withyou/study/ch02/ByteExample.java
@@ -0,0 +1,24 @@
+package com.withyou.study.ch02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		
+		
+		byte var1 = -128;
+		byte var2 = -38;
+		byte var3 = 0;
+		byte var4 = 38;
+		byte var5 = 127;
+		//byte var6 = 128; byte는 127까지만 소화가능
+		short var6 = 128;
+		
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+	}
+}
---
+++ src/com/withyou/study/ch02/CharExample.java
@@ -0,0 +1,30 @@
+package com.withyou.study.ch02;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+
+//		자바는 모든 문자를 유니코드로 처리함. 
+//		국제 표준 규약
+//		char type은 "보다는 '를 사용한다(더블보다는 싱글)//		
+		
+		
+		char c1 = 'A'; 		//문자를 저장
+		char c2 = 65;		//십진수로 저장
+		char c3 = '\u0041';	//16진수로 저장
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+
+ 		char c4 = '가'; 
+ 		char c5 = 44032; 
+ 		char c6 = '\uac00';
+ 		
+ 		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		
+	}
+}
