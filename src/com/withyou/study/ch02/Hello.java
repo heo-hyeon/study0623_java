e2bf6aace594ec69e51b1452c8758e611885c021
hyeon <gwg01051@naver.com>
2022-07-28 16:59:00 +0900

    7/28

---
+++ src/com/withyou/study/ch02/Hello.java
@@ -0,0 +1,57 @@
+package com.withyou.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		String name = "허현";
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은: " + name) ;
+		
+		
+/*		
+ *  데이터타입 변수명 = 값 ;
+ *  데이터타입 변수명
+ *  
+ *  정수 : int
+ *  실수 : double
+ *  문자 : char
+ *  블리언 : boolean : true/false 중 한개만 가질수있다.
+ *  문자열 : string
+ 
+ *		int count = 0 ; 선언및초기화
+		int count1 ;    선언
+		
+		double avg = 0.0 ;
+		
+		char nationality = "대한민국" ;
+		
+		boolean delny = true;
+		
+		String name ="장원영 이다";
+	*/
+		
+/*
+ * char bookname = "역행자";
+ * 
+ * int point = 0 ;
+ * 
+ * int recommend = 0;
+ * 
+ * String subtitle="인생 역주행 공식 대공개!" ;
+ * 
+ * String explanation = "왜 늘 돈 이야기를 하면서도 평생 돈에 허덕이는가?";
+ * 
+ * String tag = "자수성가", "라이프해커" ;
+ */
+		
+		
+		
+		
+	}
+}
