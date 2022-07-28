84947baf825329f115b6ee69979a55724cba7286
hyeon <gwg01051@naver.com>
2022-07-28 17:30:36 +0900

    7/28 commit

--- src/com/withyou/study/ch02/CharExample.java
+++ src/com/withyou/study/ch02/CharExample.java
@@ -9,22 +9,28 @@ public class CharExample {
 //		char type은 "보다는 '를 사용한다(더블보다는 싱글)//		
 		
 		
-		char c1 = 'A'; 		//문자를 저장
-		char c2 = 65;		//십진수로 저장
-		char c3 = '\u0041';	//16진수로 저장
+		char c1 = 'A'; 			//문자를 저장
+		char c2 = 65;			//십진수로 저장
+		char c3 = '\u0041';		//16진수로 저장
 		
 		System.out.println(c1);
 		System.out.println(c2);
 		System.out.println(c3);
 		
 
- 		char c4 = '가'; 
- 		char c5 = 44032; 
- 		char c6 = '\uac00';
+ 		char c4 = '가'; 			//문자를 저장
+ 		char c5 = 44032; 		//십진수로 저장
+ 		char c6 = '\uac00'; 	//16진수로 저장
  		
  		System.out.println(c4);
 		System.out.println(c5);
 		System.out.println(c6);
 		
+		int uniCode = c1;
+		System.out.println(uniCode);
+		
+		
+		int uniCode2 = c4;
+		System.out.println(uniCode2);
 	}
 }
