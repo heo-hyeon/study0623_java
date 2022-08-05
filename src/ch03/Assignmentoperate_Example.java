f2e28fc57ba5ed875b0660d0e9bd167cffc8a218
hyeon <gwg01051@naver.com>
2022-08-05 11:33:06 +0900

    8/5 연산자 마무리

---
+++ src/ch03/Assignmentoperate_Example.java
@@ -0,0 +1,25 @@
+package ch03;
+
+public class Assignmentoperate_Example {
+
+	public static void main(String[] args) {
+		
+		int result = 0;
+		int result2 = 0;
+		int result3 = 0;
+		
+		
+		result += 10;
+//		result = result + 10;
+		System.out.println ("result:" + result);
+
+		result2 -= 10;
+//		result2 = result2 -10;
+		System.out.println("result2:" + result2);
+		
+		result3 *= 10;
+//		result3 = result3 *10;
+		System.out.println ("result3:" + result3);
+		
+	}
+}
