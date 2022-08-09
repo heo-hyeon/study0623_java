a58cf76f51dde5d622627db6bc50482a910a3a0a
hyeon <gwg01051@naver.com>
2022-08-09 15:25:47 +0900

    8/9 if문

--- src/ch04/IfExample.java
+++ src/ch04/IfExample.java
@@ -19,24 +19,24 @@ public class IfExample {
 	if (score >=90) {
 		if(score >= 97) {
 			System.out.println("A+");
-				}
+		}
 		else if (score <=92 ) {
 			System.out.println("A-");
 		} else { 
 			System.out.println("A");
 		}
-			}	
+	}	
 	else if (score >=80 & score<90) {
 		if(score>=87) {
 				System.out.println("B+");
-			}
+		}
 		else if (score<=82) {
 				System.out.println("B-");
-			}
+		}
 		else {
 				System.out.println("B");
-			}
 		}
+	}
 	else if (score >= 70 & score <80) {
 		if(score >=77) {
 			System.out.println("C+");
