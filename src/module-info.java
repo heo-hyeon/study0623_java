d3383cfe628b97f67439fb2563004f00d78bfe53
hyeon <gwg01051@naver.com>
2022-09-22 17:01:39 +0900

    9/22 java

---
+++ src/module-info.java
@@ -0,0 +1,18 @@
+module study0623_java {
+	exports ch07.second;
+	exports ch05;
+	exports test;
+	exports ch06;
+	exports ch07.fifth;
+	exports ch03;
+	exports ch04;
+	exports ch07.forth;
+	exports ch02;
+	exports CodingTest;
+	exports ch07.third;
+	exports ch07.five;
+	exports ch07.sixth;
+	exports ch07.first;
+
+	requires java.desktop;
+}
